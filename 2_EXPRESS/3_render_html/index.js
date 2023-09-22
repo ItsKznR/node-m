c6af07ddaa6a956aaa5eba1bb8bf56a5625981ea
Kauã <kzndiscord@gmail.com>
2023-09-22 09:55:24 -0300

    render html com express

--- 2_EXPRESS/3_render_html/index.js
+++ 2_EXPRESS/3_render_html/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 
+const path = require('path')
+
 const app = express()
+
+
+
+const caminho = path.join(__dirname, 'templates')
 
 // GET => Envia informações do back-end para o front-end
 // POST => Recebe informações do front-end no back-end
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Este é o meu back-end")
+    resposta.sendFile(`${caminho}/index.html`)
 })
 
 app.listen(3000, () => {
